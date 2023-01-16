06c237482cc9971f783cacdc1c083fcfd3e0f427
Abyss-Reg <2638239785@qq.com>
2023-01-16 21:24:09 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   css/style.css
    	modified:   index.html
    	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var timer = new Array()
+var timerIndex = 0;
+
 function iconMove_in(){
     var ico = document.getElementById("icon");
     ico.style.left = "-30%";
@@ -48,8 +51,71 @@ function card_out(){
         window.setTimeout(function(){
             iconlip.style.width = "100px";
         },300)
-    },300);
-
-    
-    
+    },300); 
 }
+
+function homec(){
+    var dccard = document.getElementById("dccard");
+    document.getElementById("flink").style.boxShadow = "unset";
+    document.getElementById("home").style.boxShadow = "7px 5px 5px rgba(0,0,0,.8)"
+    document.getElementById("flcard").style.zIndex = "-1";
+    document.getElementById("flcard").style.opacity = "0";
+    //删除所有定时器
+    if(timerIndex != 0){
+        for (let index = 0; index < timerIndex; index++) {
+            clearInterval(timer[index]);
+            timer.splice(index,1);
+        }
+    }
+    window.setTimeout(function(){
+        dccard.style.left = "0%";
+    },200)
+    window.setTimeout(function(){
+        document.getElementById("som").style.opacity = "1";
+    },700);
+}
+
+function flinkc(){
+    var dccard = document.getElementById("dccard");
+    document.getElementById("home").style.boxShadow = "unset";
+    document.getElementById("flink").style.boxShadow = "7px 5px 5px rgba(0,0,0,.8)"
+    dccard.style.left = "-100%";
+    window.setTimeout(()=>{
+        card_out();
+    },300)
+    window.setTimeout(function(){
+        document.getElementById("som").style.opacity = "0";
+    },700);
+    window.setTimeout(function(){
+        document.getElementById("flcard").style.opacity = "1";
+    },200)
+    window.setTimeout(function(){
+        document.getElementById("flcard").style.zIndex = "1";
+    },250)
+    window.setTimeout(()=>{
+        var textbox = document.getElementsByClassName("sitename")
+            for (let index = 0; index < textbox.length; index++) {
+                var inTextBox = textbox[index].getElementsByTagName("span")[0];
+                if(inTextBox.offsetWidth>textbox[index].offsetWidth){
+                    var co = false;
+                    window.setTimeout(()=>{
+                        timer.push(setInterval(() => {
+                                if(co == false){
+                                    window.setTimeout(()=>{co = true;},1000)
+                                }
+                                var scl = (textbox[index].scrollLeft)++;
+                                if(scl == textbox[index].scrollLeft){
+                                    window.setTimeout(() =>{
+                                        textbox[index].scrollLeft = 0;
+                                        co = false;
+                                    },1000)
+                                }
+                            }, 20));
+                        timerIndex++;
+                    },1000)  
+                }
+            }
+    },250)
+}
+
+
