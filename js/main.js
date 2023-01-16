959c97e32d790530aa2be571b2f2de848e54ba56
Abyss-Reg <2638239785@qq.com>
2023-01-17 00:09:34 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   css/style.css
    	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -17,41 +17,78 @@ function card_in(){
         //已经进行过了，可能是重复触发，所以本次返回
         return;
     }
-    name_and_ico.style.width = "400px";
-    window.setTimeout(iconMove_in,300);
-    var name = document.createElement("div");
-    window.setTimeout(function(){        
-        name.className = "name";
-        name.style.bottom = "-100%";
+    if(document.getElementById("dccard").offsetWidth < 450){
+        name_and_ico.style.display = "block";
+        name_and_ico.style["transition"] = "height 0.3s";
+        name_and_ico.style.width = "auto";
+        document.getElementById("icon").style.margin = "0 auto";
+        var name = document.createElement("div");
         name.innerHTML = "<span>狐小九Little_Jiu</span>";
+        name.id = "namebar";
+        name.style.marginTop = "5px";
+        name.style.fontSize = "28px";
+        name.style.fontFamily = "SmileySans-Oblique"
+        name.style.margin = "0 auto";
+        name.style.textAlign = "center";
         name_and_ico.appendChild(name);
-    },300)
-    window.setTimeout(function(){
-        name.style.bottom = "calc(50px - 14px)";
-    },600)
-    //window.setTimeout(function(){
-        //document.getElementById("som").style.opacity = "1";
-    //},500) 
+        name_and_ico.style.height = "150px";
+    }
+    else{
+        name_and_ico.style.display = "flex";
+        name_and_ico.style["transition"] = "width 0.3s"
+        name_and_ico.style["justify-content"] = "center";
+        name_and_ico.style["align-items"] = "center";
+        name_and_ico.style.width = "400px";
+        window.setTimeout(iconMove_in,300);
+        var name = document.createElement("div");
+        window.setTimeout(function(){        
+            name.className = "name";
+            name.style.bottom = "-100%";
+            name.innerHTML = "<span>狐小九Little_Jiu</span>";
+            name_and_ico.appendChild(name);
+        },300)
+        window.setTimeout(function(){
+            name.style.bottom = "calc(50px - 14px)";
+        },600)
+    }
 }
 
 function card_out(){
     var iconlip = document.getElementsByClassName("iconlip")[0];
-    var rm = iconlip.getElementsByClassName("name")[0];
-    if(rm.style.bottom == "-100%" || iconlip.childElementCount == 1){
+    if(iconlip.childElementCount == 1){
         return;
     }
-    //window.setTimeout(function(){
-        //document.getElementById("som").style.opacity = "0";
-    //},300)
-    rm.style.bottom = "-100%";
-    window.setTimeout(function(){
-        iconlip.removeChild(rm);
-        var ico = document.getElementById("icon");
-        ico.style.left = "0";
-        window.setTimeout(function(){
+    if(document.getElementById("dccard").offsetWidth < 450){
+        var rm = document.getElementById("namebar");
+        console.log("运行到此处");
+        iconlip.style.height = "100px";
+        window.setTimeout(()=>{
+            iconlip.style["transition"] = "unset";
             iconlip.style.width = "100px";
-        },300)
-    },300); 
+        },350)
+        window.setTimeout(() => {
+            iconlip.removeChild(rm);
+            iconlip.style.display = "unset";
+            document.getElementById("icon").style.margin = "unset";
+        }, 400);
+    }
+    else{
+        var rm = iconlip.getElementsByClassName("name")[0];
+        rm.style.bottom = "-100%";
+        window.setTimeout(function(){
+            iconlip.removeChild(rm);
+            var ico = document.getElementById("icon");
+            ico.style.left = "0";
+            window.setTimeout(function(){
+                iconlip.style.width = "100px";
+                iconlip.style.display = "unset";
+                iconlip.style["transition"] = "unset"
+                iconlip.style["justify-content"] = "unset";
+                iconlip.style["align-items"] = "unset";
+            },300)
+        },300);
+    }
+     
 }
 
 function homec(){
